de29ebe782ed2b446f4b09dcb5184da053ff0b9e
ShyamJogdand <shyam.jogdand1@gmail.com>
2023-02-17 10:27:06 +0530

    JS 1st assignment committed
    JS 1st assignment committed

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+console.log("*************** 1st line ***************");
+var myFullName;
+// console.log("myFullName");
+myFullName = "Shyam Jogdand";
+console.log("My full name:", myFullName);
+myFullName = "Shyam Chudaman Jogdand";
+console.log("My full name:", myFullName);
+
+console.log("*************** 2nd line ***************");
+var myLove = "I love only JS";
+console.log("My love:", myLove);
+
+console.log("*************** 3rd line ***************");
+var firstName = "Shyam. ", lastName = "Jogdand. ", age = "33. ", address = "Nanded- Maharashtra. ", pincode = "431704. ", gender = "Male. ", isMarried = "Yes. ";
+console.log("First Name:", firstName, "Last Name:", lastName, "Age:", age, "Address:", address, "Pincode:", pincode, "Gender:", gender, "Married:", isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,25 @@
+//Personal Details
 console.log(" Shyam Jogdand ");
+console.log("31");
+console.log("Pune");
+
+
+var fullName = "Shyam Jogdand";
+console.log("My name is:",fullName);
+// console.log(fullName);
+
+var city; //Variable Declaration
+console.log("Uninitialized city name:",city);
+city = "Nanded";  // Variable initialization
+console.log("I live in:",city);
+// city = "Surat"; // Variable update
+
+
+var age = "31"; // variable declaration & initialization in same line
+age = 32;
+age = 33;
+console.log("My age is:",age);
+
+var universityName = "SRTMUN";
+console.log("My University:",universityName);
+
