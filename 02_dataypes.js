58fb8a529a340ae7a64d22b7591cbaf345b2ee4b
ShyamJogdand <shyam.jogdand1@gmail.com>
2023-02-20 10:47:45 +0530

    pending commits
    pending commits

---
+++ 02_dataypes.js
@@ -0,0 +1,38 @@
+var myName = "Virat Kohli";
+var fatherName = "Yogesh Kohli";
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false;
+
+var typeOfMyName = typeof myName;
+console.log("Value of variable myName:", myName);
+console.log("Type of variable myName:", typeOfMyName);
+
+console.log("Value of variable fatherName:", fatherName);
+console.log("Type of variable fatherName:", typeof fatherName);
+
+var typeOfAge = typeof age;
+console.log("Value of variable age:", age);
+console.log("Type of variable age:", typeOfAge);
+
+console.log("Value of variable weight:", weight);
+console.log("Type of variable weight:", typeof weight);
+
+console.log("Value of variable isMarried:", isMarried);
+console.log("Type of variable isMarried:", typeof isMarried);
+
+console.log("Value of variable isLazy:", isLazy);
+console.log("Type of variable isLazy:", typeof isLazy);
+
+var graduation;
+console.log("Value of graduaction variable is:", graduation);
+console.log("Type of graduaction variable is:", typeof graduation);
+
+var city = null;
+console.log("Value of city variable is:", city);
+console.log("Type of city variable is:", typeof city);
+
+age = "32";
