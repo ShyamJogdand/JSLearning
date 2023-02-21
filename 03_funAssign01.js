a6d092b351fedd22f1de0a959f91935653f6ebb1
ShyamJogdand <shyam.jogdand1@gmail.com>
2023-02-21 12:03:24 +0530

    assignment updated
    assignment updated

--- 03_funAssign01.js
+++ 03_funAssign01.js
@@ -12,24 +12,24 @@ simpleFuction2();
 console.log("********** Function with args inside funtion & no return value **********");
 function personalDetials(firstName, lastName, collegeName){
     firstName = "Shyam";
-    console.log("First Name   : ", firstName);
+    console.log("First Name   :", firstName);
     lastName = "Jogdand";
-    console.log("Last Name    : ", lastName);
+    console.log("Last Name    :", lastName);
     collegeName = "Modern College";
-    console.log("College Name : ", collegeName);
+    console.log("College Name :", collegeName);
 }
 personalDetials();
 
 console.log("********** Swap function with two args & no return value **********");
 function swapValuesDude(temp1, temp2){
-    console.log("Befoe swap: ", temp1);
-    console.log("Befoe swap: ", temp2);
+    console.log("Before swap    :", temp1);
+    console.log("Before swap    :", temp2);
     var temp3;
     temp3 = temp1;
     temp1 = temp2;
     temp2 = temp3;
-    console.log("After swap: ", temp1);
-    console.log("After swap: ", temp2);
+    console.log("After swap     :", temp1);
+    console.log("After swap     :", temp2);
 }
 swapValuesDude("Virat", "Anushka");
 swapValuesDude(1000, 2000);
@@ -37,7 +37,7 @@ swapValuesDude(1000, 2000);
 console.log("********** Function with args & return value **********");
 function addThreeValues(num1, num2, num3){
     var sum = num1 + num2 + num3;
-    console.log("Sum of given values : ", sum);
+    console.log("Sum of given values :", sum);
     return sum;
 }
 var sumRusult = addThreeValues(10.23, 600, 40);
