10fdf414becb2fe3484074526663c4a0aa43dfc9
ShyamJogdand <shyam.jogdand1@gmail.com>
2023-03-03 16:30:42 +0530

    Assignment updated & committed
    Assignment updated & committed

--- 06_factorial_B.js
+++ 06_factorial_B.js
@@ -1,7 +1,10 @@
-console.log("--------------- Factorial of a String ----------");
+console.log("--------------- Factorial of total words of a String ----------");
 function factorialOfNum(fStrng){
-    let strLength = fStrng.length;
-    console.log(`Given string is "${fStrng}" & its length is: ${strLength}`);
+    
+    let strLength = fStrng.split(" ").length;
+    console.log("   ");
+
+    console.log(`Given string is "${fStrng}" of : ${strLength} words`);
     let fac =1;
     for (strLength; strLength >= 1; strLength--) {
         fac = fac * strLength;
@@ -9,22 +12,22 @@ function factorialOfNum(fStrng){
     return fac;
 }
 var result = factorialOfNum("Revision is the mother of Success");
-console.log(`Factorial of given string's length is          : ${result}`);
+console.log(`Factorial of given string's total words is          : ${result}`);
 
 var result = factorialOfNum("We never fail, we always learn");
-console.log(`Factorial of given string's length is          : ${result}`);
+console.log(`Factorial of given string's total words is          : ${result}`);
 
 var result = factorialOfNum("Shyam Chudaman Jogdand");
-console.log(`Factorial of given string's length is          : ${result}`);
+console.log(`Factorial of given string's total words is          : ${result}`);
 
 var result = factorialOfNum("");
-console.log(`Factorial of given string's length is          : ${result}`);
+console.log(`Factorial of given string's total words is          : ${result}`);
 
-var result = factorialOfNum(NaN);
-console.log(`Factorial of given string's length is          : ${result}`);
+// var result = factorialOfNum(NaN);
+// console.log(`Factorial of given string's total words is       : ${result}`); // giving an error
 
 // var result = factorialOfNum(null);
-// console.log(`Factorial of given string's length is          : ${result}`); // giving an error
+// console.log(`Factorial of given string's total words is       : ${result}`); // giving an error
 
 // var result = factorialOfNum(undefined);
-// console.log(`Factorial of given string's length is          : ${result}`); // giving an error
+// console.log(`Factorial of given string's total words is       : ${result}`); // giving an error
